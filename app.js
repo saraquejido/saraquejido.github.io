f7eb42dda5059cb11316db6267fd514a80cefff4
Sara Quejido <quejidosara@gmail.com>
2020-06-10 10:19:18 +0200

    Date added in JS

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
 
-console.log('Here\'s a hidden message');
-
-today = new Date();
-formatDate = today.toDateString();
-selectElement = document.getElementById('date');
-selectElement.innerHTML = formatDate;
+function {
+    let today = new Date();
+    let formatDate = today.toDateString();
+    let selectElement = document.getElementById('date');
+    selectElement.innerHTML = formatDate;
+}
