3b98ea1c6f1e02bbf2432787e486eb5f1debde27
Sara Quejido <quejidosara@gmail.com>
2020-06-17 11:27:32 +0200

    JS date changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 let today = new Date();
 let formatDate = today.toDateString();
-let selectElement = document.getElementById('date');
-selectElement.innerHTML = formatDate;
+document.getElementById("date");
+
